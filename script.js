c39678f7ae109784b0288fda146064478c7c25f4
Jibé Barth <barth.jib@gmail.com>
2016-02-26 14:30:14 +0100

    Split commit message in multiple link when issue found

--- script.js
+++ script.js
@@ -4,15 +4,18 @@
 const _BUGTRACKER_ISSUE_URL_ = "http://redmine.microclimat.com/issues/"
 
 $('.commit-title').each(function(){
+    var mythis = $(this);
     var content = $(this).text();
     var aContent = content.split(/(#[1-9\d-]+)/);
     if(aContent.length > 1) {
         console.log(aContent);
-        var oLink = $(this).find('a');
-        $(this).html();
+        var oLink = $(this).find('a')[0];
+        $(mythis).html('');
         for (var i = 0; i < aContent.length; i++) {
-            oLink.text(aContent[i]);
-            $(this).append(oLink);
+            var newLink = oLink;
+            $(newLink).text(aContent[i]);
+            $(newLink).clone().appendTo($(mythis));
+            newLink = null;
         }
     }
 })
